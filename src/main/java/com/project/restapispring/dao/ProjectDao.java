db06d2381414a08b34473bfc44bd15c465960b20
Mosad-Sayed <55459416+Mosad-Sayed@users.noreply.github.com>
2022-05-26 18:00:58 +0200

    enhance the code

--- src/main/java/com/project/restapispring/dao/ProjectDao.java
+++ src/main/java/com/project/restapispring/dao/ProjectDao.java
@@ -1,9 +1,10 @@
 package com.project.restapispring.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
 import com.project.restapispring.model.Project;
 
+@Repository
 public interface ProjectDao extends JpaRepository<Project, Integer> {
 
 }
